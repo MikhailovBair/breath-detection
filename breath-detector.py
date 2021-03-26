9b2ba5ed1ba02f9181def15c7f6ee58c61302dc3
bair <bair1209@gmail.com>
2021-03-27 02:04:07 +0300

    improved json shortcut fixed coefficents

--- breath-detector.py
+++ breath-detector.py
@@ -5,12 +5,12 @@ import sys
 from matplotlib import pyplot as plt
 from scipy.fft import fft, fftfreq
 
+
+DETECTION_CHANGE_PERIOD = 120
 name = sys.argv[1]
 extension = sys.argv[2]
 should_not_process_video = int(sys.argv[3])
-print(name)
 video_name = "videos/" + name + "." + extension
-times_name = "times_" + name + ".json"
 breath_changes_name = "breath_changes_" + name + ".json"
 cap = cv2.VideoCapture(video_name)
 
@@ -22,23 +22,23 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 
 chest_sizes = np.array([0])
-times = np.array([0])
 breath_changes = np.array([0])
-
-i = 0
-x, y, w, h = 0, 0, 0, 0
-last_time = -10000
 
 
 def process_video():
+    x, y, w, h = 0, 0, 0, 0
+    i = 0
+    last_time = -DETECTION_CHANGE_PERIOD
     while True:
-        global i, last_time, chest_sizes, times, breath_changes
+        global chest_sizes, breath_changes
         i += 1
         ret, frame = cap.read()
         if not ret:
+            with open(breath_changes_name, "w") as write_file:
+                json.dump(breath_changes.tolist(), write_file)
             break
 
-        if i - last_time >= 120:
+        if i - last_time >= DETECTION_CHANGE_PERIOD:
             faces = face_cascade.detectMultiScale(frame, 1.1, 10, minSize=(100, 100))
             if faces is None or len(faces) == 0:
                 print('Failed to detect face')
@@ -47,19 +47,16 @@ def process_video():
             last_time = i
 
         frame = cv2.GaussianBlur(frame, (7, 7), 0)
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         crop_img = frame[y + round(1.2 * h): y + round(3 * h), x - round(w * 0.3): x + round(w * 1.4)]
 
-        sz = np.shape(crop_img)
         mean = np.mean(crop_img, (0, 1), keepdims=True)
         cnt = np.sum((np.sum(np.abs(crop_img - mean), axis=2) < 40))
 
         chest_sizes = np.append(chest_sizes, cnt / (np.shape(crop_img)[0] * np.shape(crop_img)[1]))
         if abs(chest_sizes[-1] - chest_sizes[-2]) < 0.01:
             breath_changes = np.append(breath_changes, breath_changes[-1] + chest_sizes[-1] - chest_sizes[-2])
-            times = np.append(times, i / 30)
 
-        ax.plot(times, breath_changes)
+        ax.plot(np.arange(len(breath_changes)), breath_changes)
         fig.canvas.draw()
         plot_img_np = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
         plot_img_np = plot_img_np.reshape(fig.canvas.get_width_height()[::-1] + (3,))
@@ -67,30 +64,17 @@ def process_video():
         cv2.imshow('Graph', plot_img_np)
         cv2.imshow('Crop', crop_img)
 
-        print(i / 30, i)
-
-        if i % 100 == 0:
-            with open(breath_changes_name, "w") as write_file:
-                json.dump(breath_changes.tolist(), write_file)
-
-            with open(times_name, "w") as write_file:
-                json.dump(times.tolist(), write_file)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 
 def get_frequency():
-    global times, breath_changes
+    global breath_changes
     if should_not_process_video:
-        with open(times_name, "r") as read_file:
-            times = np.array(json.load(read_file)) * 30
-        times = np.round(times).astype('int64')
-
         with open(breath_changes_name, "r") as read_file:
             breath_changes = np.array(json.load(read_file))
 
-    plt.plot(times, breath_changes)
+    plt.plot(np.arange(len(breath_changes)), breath_changes)
     plt.figure(figsize=(12, 12))
     one_chunk = np.tile(breath_changes, 10)
     y = fft(one_chunk - one_chunk.mean())
@@ -99,7 +83,7 @@ def get_frequency():
     plt.xlim(.1, 3)
     plt.ylim(0, 100)
     plt.show()
-    mask = (x >= .27)
+    mask = (x >= .29)
     frequency = x[mask][abs(y[mask]).argmax()]
     return frequency
 
@@ -112,4 +96,4 @@ cv2.destroyAllWindows()
 
 
 freq = get_frequency()
-print(freq, len(times) / 30 * freq)
+print(freq, len(breath_changes) / 30 * freq)
