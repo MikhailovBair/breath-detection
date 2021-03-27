f05e206234fbdec49837628d2632de3b48076d09
Андрей Головацкий <andi@Andrejs-MacBook-Pro.local>
2021-03-27 10:22:20 +0300

    style

--- breath_detector.py
+++ breath_detector.py
@@ -45,7 +45,7 @@ def process_video():
         if i - last_time >= DETECTION_CHANGE_PERIOD:
             faces = face_cascade.detectMultiScale(frame, 1.1, 10, minSize=(100, 100))
             if faces is None or len(faces) == 0:
-                print('Failed to detect face')
+                print("Failed to detect face")
                 continue
             x, y, w, h = faces[0]
             last_time = i
@@ -58,18 +58,36 @@ def process_video():
             frames = frames[1:] + [face]
 
         frame = cv2.GaussianBlur(frame, (7, 7), 0)
-        crop_img = frame[y + round(1.2 * h): y + round(3 * h), x - round(w * 0.3): x + round(w * 1.4)]
+        crop_img = frame[
+            y + round(1.2 * h) : y + round(3 * h),
+            x - round(w * 0.3) : x + round(w * 1.4),
+        ]
         frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
-        frame = cv2.rectangle(frame, (x - round(w * 0.3), y + round(1.2 * h)), (x + round(w * 1.4), y + round(3 * h)),
-                              (255, 255, 255), 2)
-        frame = cv2.rectangle(frame, (x + w // 3, y + h // 15), (x + 2 * w // 3, y + h // 5), (255, 255, 255), 2)
+        frame = cv2.rectangle(
+            frame,
+            (x - round(w * 0.3), y + round(1.2 * h)),
+            (x + round(w * 1.4), y + round(3 * h)),
+            (255, 255, 255),
+            2,
+        )
+        frame = cv2.rectangle(
+            frame,
+            (x + w // 3, y + h // 15),
+            (x + 2 * w // 3, y + h // 5),
+            (255, 255, 255),
+            2,
+        )
 
         mean = np.mean(crop_img, (0, 1), keepdims=True)
         cnt = np.sum((np.sum(np.abs(crop_img - mean), axis=2) < 40))
 
-        chest_sizes = np.append(chest_sizes, cnt / (np.shape(crop_img)[0] * np.shape(crop_img)[1]))
+        chest_sizes = np.append(
+            chest_sizes, cnt / (np.shape(crop_img)[0] * np.shape(crop_img)[1])
+        )
         if abs(chest_sizes[-1] - chest_sizes[-2]) < 0.01:
-            breath_changes = np.append(breath_changes, breath_changes[-1] + chest_sizes[-1] - chest_sizes[-2])
+            breath_changes = np.append(
+                breath_changes, breath_changes[-1] + chest_sizes[-1] - chest_sizes[-2]
+            )
 
         if i % 10 == 0:
             sz = len(breath_changes)
@@ -82,18 +100,28 @@ def process_video():
                 breath = get_frequency()
 
         cv2.rectangle(frame, (0, 0), (400, 100), (255, 255, 255), cv2.FILLED)
-        cv2.putText(frame, "pulse: {:.2f}".format(pulse), (10, 30),
-                    cv2.FONT_ITALIC,
-                    1.1,
-                    (255, 0, 0), 2)
-        cv2.putText(frame, "breathing rate: {:.2f}".format(breath), (10, 60),
-                    cv2.FONT_ITALIC,
-                    1.1,
-                    (0, 255, 0), 2)
+        cv2.putText(
+            frame,
+            "pulse: {:.2f}".format(pulse),
+            (10, 30),
+            cv2.FONT_ITALIC,
+            1.1,
+            (255, 0, 0),
+            2,
+        )
+        cv2.putText(
+            frame,
+            "breathing rate: {:.2f}".format(breath),
+            (10, 60),
+            cv2.FONT_ITALIC,
+            1.1,
+            (0, 255, 0),
+            2,
+        )
 
-        cv2.imshow('Frame', frame)
+        cv2.imshow("Frame", frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
 
@@ -103,12 +131,12 @@ def get_frequency(graph=False):
         with open(breath_changes_name, "r") as read_file:
             breath_changes = np.array(json.load(read_file))
 
-    #plt.plot(np.arange(len(breath_changes)), breath_changes)
+    # plt.plot(np.arange(len(breath_changes)), breath_changes)
     #
     one_chunk = np.tile(breath_changes, 10)
     y = fft(one_chunk - one_chunk.mean())
     x = fftfreq(len(one_chunk), 1 / FPS)
-    mask = (x >= .28)
+    mask = x >= 0.28
     frequency = x[mask][abs(y[mask]).argmax()]
     return frequency
 
@@ -125,21 +153,28 @@ def make_verdict(freq, times):
     while True:
         ans = np.zeros((130, 900, 3))
 
-        cv2.putText(ans, "Your breath rate is: {:.2} breathes per sec".format(freq), (10, 30),
-                    cv2.FONT_ITALIC,
-                    1.1,
-                    (255, 0, 0), 2)
-        cv2.putText(ans, "You have breathed {:.0f} times".format(times / FPS * freq), (10, 60),
-                    cv2.FONT_ITALIC,
-                    1.1,
-                    (0, 255, 0), 2)
-        cv2.putText(ans, verdict, (10, 90),
-                    cv2.FONT_ITALIC,
-                    1.1,
-                    (0, 0, 255), 2)
-        cv2.imshow('Frame', ans)
+        cv2.putText(
+            ans,
+            "Your breath rate is: {:.2} breathes per sec".format(freq),
+            (10, 30),
+            cv2.FONT_ITALIC,
+            1.1,
+            (255, 0, 0),
+            2,
+        )
+        cv2.putText(
+            ans,
+            "You have breathed {:.0f} times".format(times / FPS * freq),
+            (10, 60),
+            cv2.FONT_ITALIC,
+            1.1,
+            (0, 255, 0),
+            2,
+        )
+        cv2.putText(ans, verdict, (10, 90), cv2.FONT_ITALIC, 1.1, (0, 0, 255), 2)
+        cv2.imshow("Frame", ans)
 
-        if cv2.waitKey(0) & 0xFF == ord('q'):
+        if cv2.waitKey(0) & 0xFF == ord("q"):
             break
 
 
--- pulse_detector.py
+++ pulse_detector.py
@@ -10,13 +10,13 @@ def pulse_from_breath(breath_frequency):
 
 def crop_fragment(img, face):
     x, y, w, h = face
-    return img[y + h // 15: y + h // 5, x + w // 3: x + 2 * w // 3]
+    return img[y + h // 15 : y + h // 5, x + w // 3 : x + 2 * w // 3]
 
 
 def moving_average(a, n=3):
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
+    return ret[n - 1 :] / n
 
 
 def pulse_from_sample(sample, rotate=False, fps=30):
@@ -26,5 +26,5 @@ def pulse_from_sample(sample, rotate=False, fps=30):
     fft_y = rfft(sample)
     fft_x = rfftfreq(len(sample), 1 / fps)
 
-    mask = (fft_x >= .8) & (fft_x <= 3.7)
+    mask = (fft_x >= 0.8) & (fft_x <= 3.7)
     return fft_x[mask][abs(fft_y[mask]).argmax()]
